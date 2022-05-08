1e53f5e22fc35b93b30ae1212d814afd3b8a7770
Sheravgan Mirov <mirov.she@gmail.com>
2022-05-08 23:22:45 +0500

    add redux tailwindcss react-router-dom@v6

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='container mx-auto p-4'>
+            <h1 className='text-4xl text-white rounded text-center p-8 bg-fuchsia-400 font-bold border-b-2 border-b-blue-300 bg-blue-200'>Hello
+                World
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
